3e975ed134d51add026b2adcfc4fd313535f1957
Mustafa <info@glanexdigital.com>
2026-03-23 02:12:45 +0300

    Initial V1 setup: clean project, Firebase integrated, env configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env"); // 🔥 BURASI
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
